53595796955d82d87c14db254d29668c780f5207
JonathanCouck <78811390+JonathanCouck@users.noreply.github.com>
2021-10-28 19:15:00 +0200

    add mock_data

---
+++ src/mock_data/place_mock.js
@@ -0,0 +1,22 @@
+const PLACES_DATA = [
+  {
+    id: "a521ea17-81a0-462c-946d-177292d57617",
+    name: "Grand Casino de Namur",
+    country: "Belgium",
+    city: "Namur",
+    postal_code: "5000",
+    street: "Avenue Baron de Moreau",
+    house_number: 1
+  },
+  {
+    id: "77e7e385-610a-4d7f-a203-92166de7d946",
+    name: "Pokahnights",
+    country: "Belgium",
+    city: "Antwerpen",
+    postal_code: "2610",
+    street: "Boomsesteenweg",
+    house_number: 440
+  }
+];
+
+export default PLACES_DATA;
---
+++ src/mock_data/user_mock.js
@@ -0,0 +1,25 @@
+const USER_DATA = [
+  {
+    id: "17707541-c6c4-407a-955d-b55d8db41a19",
+    first_name: "Guido",
+    last_name: "Couck",
+    birth_date: "1958-08-28T23:00:00.000Z",
+    favorite_place_id: "a521ea17-81a0-462c-946d-177292d57617"
+  },
+  {
+    id: "38fb2aac-544f-47a5-8658-6273936a9a2b",
+    first_name: "Jonathan",
+    last_name: "Couck",
+    birth_date: "1999-04-15T22:00:00.000Z",
+    favorite_place_id: "77e7e385-610a-4d7f-a203-92166de7d946"
+  },
+  {
+    id: "48e02f19-f502-487f-969e-96ed5de57009",
+    first_name: "Daniel",
+    last_name: "Negreanu",
+    birth_date: "1974-07-25T23:00:00.000Z",
+    favorite_place_i: "77e7e385-610a-4d7f-a203-92166de7d946"
+  }
+];
+
+export default USER_DATA;
