8e3256276f2a18ab975f7d560124271ebc0c5819
JonathanCouck <78811390+JonathanCouck@users.noreply.github.com>
2021-10-28 16:38:51 +0200

    Restart everything

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import MainComponent from './components/MainComponent';
-import Navbar from './components/Navbar';
-import USER_DATA from './data/user_mock'
-import TOURNAMENT_DATA from './data/tournament_mock'
-import CASHGAME_DATA from './data/cashgame_mock'
 
 function App() {
-  const [users, setUsers] = useState(USER_DATA);
-  const [tournaments, setTournaments] = useState(TOURNAMENT_DATA);
-  const [cashgames, setCashgames] = useState(CASHGAME_DATA);
-  console.log(users)
-  console.log(tournaments)
-  console.log(cashgames)
   return (
     <div className="App">
-      <Navbar id='0'/>
-      <MainComponent/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
