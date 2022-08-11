abbe104ba0d3e8c9c8c96d348d197bfaff2244f5
JonathanCouck <78811390+JonathanCouck@users.noreply.github.com>
2022-08-11 12:03:08 +0200

    translate index file

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { useTranslation }from 'react-i18next'
 
 const Index = () => {
+  const { t } = useTranslation();
   return (
     <div className='m-5'>
-      <h1 className='text-2xl font-semibold mb-2'>Poker Tracker site</h1>
-      <span>Log in to start tracking your poker results</span>
+      <h1 className='text-2xl font-semibold mb-2'>{t('Index.trackerSite')}</h1>
+      <span>{t('Index.logTrack')}</span>
     </div>
   )
 }
